904a17824d73d681faa3ceb79f1557ae5d63426c
Mayanaollivier <mayana.ollivier@gmail.com>
2020-08-07 14:22:35 -0300

    Adicionando entidades CC e CP (e seus atributos)

---
+++ Projeto_Banco_java1_final/src/entidades/ContaPoupanca.java
@@ -0,0 +1,15 @@
+package entidades;
+
+public class ContaPoupanca {
+
+	private String tipo;
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	} 
+	
+}
